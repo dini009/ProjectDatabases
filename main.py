c8e548262b2b1ccebea3efbcc4d48b4d975d31f6
dini009 <74607659+dini009@users.noreply.github.com>
2021-04-12 12:06:11 +0200

    Update main.py
    This is just the Front end. Will update regularly to finish.

---
+++ main.py
@@ -0,0 +1,65 @@
+#The FrontEnd
+
+
+from tkinter import *
+import tkinter.messagebox
+
+
+class Student:
+
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Student Database Management System")
+        self.root.geometry("1350x7500+0+0")
+        self.root.config(bg = "cadet blue")
+
+      
+
+        
+
+        student_ID = StringVar()
+        Firstname = StringVar()
+        Surname = StringVar()
+        DoB = StringVar()
+        Age = StringVar()
+        Gender = StringVar()
+        Grades = StringVar()
+        Nationality = StringVar()
+        Street_Name = StringVar()
+        postal_Code = StringVar()
+        City = StringVar()
+        phone_number = StringVar()
+        Email_address = StringVar()
+        Study_Name = StringVar()
+        Year_of_Study = StringVar()
+        Study_counsellor = StringVar()
+
+        
+
+
+
+
+    
+    if __name__ == '__main__':
+        root = Tk()
+        root.mainloop()
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
