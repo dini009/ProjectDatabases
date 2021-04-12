16955e4d8d8d25a503bd33a3f9b760cb324b9b42
dini009 <74607659+dini009@users.noreply.github.com>
2021-04-12 20:22:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,63 +4,50 @@
 from tkinter import *
 import tkinter.messagebox
 
+class Student:
 
-class Application:
-
-    def __init__(self, root):
+    def __init__(self,root):
         self.root = root
-        self.root.title("")
+        self.root.title("Database system")
         self.root.geometry("1350x750+0+0")
         self.root.config(bg = "#0000ff")
 
-      
-
-        
-
-        StdID = StringVar()
+    #Creating variables and assigning each one.
         Firstname = StringVar()
         Surname = StringVar()
-        DoB = StringVar()
+        Dob = StringVar()
         Age = StringVar()
-        Gender = StringVar()
-        Grades = StringVar()
-        Nationality = StringVar()
-        Street_Name = StringVar()
-        postal_Code = StringVar()
-        City = StringVar()
-        phone_number = StringVar()
-        Email_address = StringVar()
-        Study_Name = StringVar()
-        Year_of_Study = StringVar()
-        Study_counsellor = StringVar()
-
-        
+        Mobile = StringVar()
+        Post_Code = StringVar()
 
 
 
+        Basic_Frame = Frame(self.root, bg = "#0000ff")
+        Basic_Frame.grid()
+
+        Title_Frame = Frame(Basic_Frame, bd = 2, padx = 54, pady = 8, bg= "#FFFFFF", relief = RIDGE)
+        Title_Frame.pack(side=TOP)
+
+
+        self.Title_label = Label(Title_Frame, font = ('arial, 40, bold'), text = "Database management", bg = "white")
+        self.Title_label.grid()
+
+        Frame_Button = Frame(Basic_Frame, bd=2, width = 1350, height = 70, padx = 18, pady=10, bg = "white", relief=RIDGE)
+        Frame_Button.pack(side = BOTTOM)
+
+        DataFrame = Frame(Basic_Frame, bd = 1, width = 1300, height = 600, padx = 20, relief=RIDGE, bg = "white")
+        DataFrame.pack(side = BOTTOM)
+
+        DataFrameLEFT = LabelFrame(DataFrame, bd=1, width = 1000, height = 600, padx = 20, relief=RIDGE, bg = "blue")
+        DataFrameLEFT.pack(side = LEFT)
+
+        DataFrameRIGHT = LabelFrame(DataFrame, bd=1, width = 450, height = 200, padx = 31, pady=3, relief=RIDGE, BG = "white")
+        DataFrameRIGHT.pack(side = RIGHT)
+
+
+if __name__=='__main__':
+    root = Tk()
+    application = Student(root)
+    root.mainloop()
 
     
-root = Tk()
-app= Application(root)
-root.mainloop()
-
-    
-
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
