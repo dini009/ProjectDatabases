15d35a1f2024775e8a8f16e46768fb1b66c09123
dini009 <74607659+dini009@users.noreply.github.com>
2021-04-12 21:03:11 +0200

    Updated.

--- main.py
+++ main.py
@@ -4,45 +4,41 @@
 from tkinter import *
 import tkinter.messagebox
 
+class Student:
 
-class Application:
-
-    def __init__(self, root):
+    def __init__(self,root):
         self.root = root
-        self.root.title("")
+        self.root.title("Database system")
         self.root.geometry("1350x750+0+0")
         self.root.config(bg = "#0000ff")
 
-      
-
-        
-
-        StdID = StringVar()
+        #Creating variables and assigning each one an empty value so user can enter their own values.
         Firstname = StringVar()
         Surname = StringVar()
-        DoB = StringVar()
+        Dob = StringVar()
         Age = StringVar()
-        Gender = StringVar()
-        Grades = StringVar()
-        Nationality = StringVar()
-        Street_Name = StringVar()
-        postal_Code = StringVar()
-        City = StringVar()
-        phone_number = StringVar()
-        Email_address = StringVar()
-        Study_Name = StringVar()
-        Year_of_Study = StringVar()
-        Study_counsellor = StringVar()
+        Mobile = StringVar()
+        Post_Code = StringVar()
+
+
+
+        Basic_Frame = Frame(self.root, bg = "#0000ff")
+        Basic_Frame.grid()
+
+        Title_Frame = Frame(Basic_Frame, bd = 2, padx = 54, pady = 8, bg= "#FFFFFF", relief = RIDGE)
+        Title_Frame.pack(side=TOP)
+
+
+        self.Title_label = Label(Title_Frame, font = ('arial', 40, 'bold'), text = "Database management System", bg = "white")
+        self.Title_label.grid()
 
         
 
 
-
-
-    
-root = Tk()
-app= Application(root)
-root.mainloop()
+if __name__=='__main__':
+    root = Tk()
+    application = Student(root)
+    root.mainloop()
 
     
 
